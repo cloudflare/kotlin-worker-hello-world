8c574d8e5c8c4594a9a53e8ac42951f0861ab6c7
Salvatore Testa <sal@sal.dev>
2022-09-13 10:28:32 -0700

    Update for Wrangler 2 and Kotlin 1.7.10
    This uses the newer Worker syntax and removes some of the bespoke
    browser work now that Kotlin better supports JavaScript.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.3.72"
+    kotlin("js") version "1.7.10"
 }
 
 group = "org.example"
@@ -9,19 +9,10 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib-js"))
-    testImplementation(kotlin("test-js"))
-}
-
-kotlin.target.browser {
-}
-
-
-tasks.register("buildWorker") {
-    dependsOn("browserProductionWebpack")
-    doLast {
-        /* Kotlin js output assumes window exists, which it won't on Workers. Hack around it */
-        file("$projectDir/index.js").writeText("const window=this; " + file("$projectDir/build/distributions/${rootProject.name}.js").readText())
+kotlin {
+    js(IR) {
+        nodejs {
+        }
+        binaries.executable()
     }
 }
